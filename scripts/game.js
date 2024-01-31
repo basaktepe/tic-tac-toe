78195a605ac2d9f80c59c3c4da045fa5819eef97
Başak Tepe <100874056+basaktepe@users.noreply.github.com>
2024-01-31 15:51:37 +0300

    Update game.js

--- scripts/game.js
+++ scripts/game.js
@@ -24,7 +24,7 @@ function startNewGame(event) {
     return;
   }
 
-  resetGameStatus();
+  //resetGameStatus();
 
   activePlayerNameElement.textContent = players[activePlayer].name;
   activeGameElement.style.display = "block";
